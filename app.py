dba91578e6a17ed93ad0ef7e40a2400771aefc3f
Salomé Ribba <Dr4x14913>
2025-06-27 14:00:55 +0200

    change google oauth scope

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class FlaskSessionCacheHandler:
 GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI', 'http://localhost:8050/callback')
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
-GOOGLE_SCOPES = ['https://www.googleapis.com/auth/youtube']
+GOOGLE_SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 if "https" not in GOOGLE_REDIRECT_URI:
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
